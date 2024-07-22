da6bd6c4addb981c7ab6b7d5dfc616324f39d9ba
khurshid <Khurshid@qurilo.com>
2024-07-22 11:37:31 +0530

    update

--- routes/campaign.js
+++ routes/campaign.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {createCampaign} = require("../controllers/createCampaign");
+const {allCampaign} = require("../controllers/allCampaign");
 const upload = require("../middlewares/upload");
 
-
+// route to create campaign
 router.post('/create',upload.array('creatives'),createCampaign);
 
+// route to fetch all campaigns data 
+router.get('/allcampaigns',allCampaign)
 
 module.exports = router;
